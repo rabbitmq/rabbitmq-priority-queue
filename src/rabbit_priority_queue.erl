2688ccb444616c87a54c7eecc5b89ba3474716a8
Simon MacMullen <simon@rabbitmq.com>
2014-05-20 15:49:05 +0100

    Fix a fairly glaring bug in clean non-prioritised durable queue recovery.

--- src/rabbit_priority_queue.erl
+++ src/rabbit_priority_queue.erl
@@ -118,8 +118,9 @@ priorities(#amqqueue{arguments = Args}) ->
 init(Q, Recover, AsyncCallback) ->
     BQ = bq(),
     case priorities(Q) of
-        none -> #passthrough{bq  = BQ,
-                             bqs = BQ:init(Q, Recover, AsyncCallback)};
+        none -> [RealRecover] = Recover, %% [0]
+                #passthrough{bq  = BQ,
+                             bqs = BQ:init(Q, RealRecover, AsyncCallback)};
         Ps   -> Init = fun (P, Term) ->
                                BQ:init(
                                  mutate_name(P, Q), Term,
@@ -133,6 +134,10 @@ init(Q, Recover, AsyncCallback) ->
                 #state{bq   = BQ,
                        bqss = BQSs}
     end.
+%% [0] collapse_recovery has the effect of making a list of recovery
+%% terms in priority order, even for non priority queues. It's easier
+%% to do that and "unwrap" in init/3 than to have collapse_recovery be
+%% aware of non-priority queues.
 
 terminate(Reason, State = #state{bq = BQ}) ->
     foreach1(fun (_P, BQSN) -> BQ:terminate(Reason, BQSN) end, State);
